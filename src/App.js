4f1141d9ce00a3b21c3839b9832a509352e7ff37
Sadman600 <sadmansakib600@gmail.com>
2022-04-19 13:04:40 +0600

    added header component and routes in app.js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Pages/Header/Header/Header';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
+      <Routes>
+        <Route></Route>
+      </Routes>
     </div>
   );
 }
